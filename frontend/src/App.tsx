e9834c203348405f6fff48f11854e7f614b05883
Eloize Eliot <q.eloize@escola.pr.gov.br>
2026-09-17 11:08:50 -0300

    Frontend parte 2 - tsconfig, vite, App

---
+++ frontend/src/App.tsx
@@ -0,0 +1,58 @@
+import { useEffect } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthStore } from './lib/store'
+import { Layout } from './components/layout/Layout'
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import { Wardrobe } from './pages/Wardrobe'
+import { Generate } from './pages/Generate'
+import { Looks } from './pages/Looks'
+
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const user = useAuthStore((s) => s.user)
+  if (!user) return <Navigate to="/login" replace />
+  return <>{children}</>
+}
+
+export default function App() {
+  const hydrate = useAuthStore((s) => s.hydrate)
+
+  useEffect(() => {
+    hydrate()
+  }, [hydrate])
+
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/wardrobe"
+          element={
+            <ProtectedRoute>
+              <Wardrobe />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/generate"
+          element={
+            <ProtectedRoute>
+              <Generate />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/looks"
+          element={
+            <ProtectedRoute>
+              <Looks />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
+    </Routes>
+  )
+}
