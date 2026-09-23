55d2d1c2842b1e169b29ced61d2a33d4c6193548
Eloize Eliot <q.eloize@escola.pr.gov.br>
2026-09-23 09:38:57 -0300

    Registra rota de clima

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes import auth, clothing, looks
+from app.api.routes import auth, clothing, looks, weather
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
 api_router.include_router(clothing.router)
 api_router.include_router(looks.router)
+api_router.include_router(weather.router)
